e099b38f8184f10149c5a1b73802c9f86f7b2f02
Anthony Pham <khanhdinhpham@gmail.com>
2019-12-30 02:40:46 -0800

    Update: Test Prototype

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,54 @@
 $(document).ready(function () {
     var ws_data = [
-        ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
-        ['1', 'NGUYỄN THỊ BẢO HẰNG', '8005741990', '29,872,331', '5,100,000', '19,644,429', '2,563,951', '2,563,951', '535,500', '5', '0', '0'],
+        ['Họ và tên', 'Lương']
     ];
 
-    var container = document.querySelector("#spreadsheet");
-    const hot = new Handsontable(container, {
-        data: ws_data,
-        rowHeaders: true,
-        colHeaders: true,
-        filters: true,
-        dropdownMenu: true,
-        licenseKey: 'non-commercial-and-evaluation'
+    // Add new person salary profile
+    $("#save").click(function () {
+        // Reset person profile
+        let person = [];
+        // Update person profile
+        person.push($("#name").val());
+        person.push($("#salary").val());
+        // Add new person profile to existing data table
+        ws_data.push(person);
+
+        // Display data in data table
+        let display = "<tr><td>" + $("#name").val() + "</td><td>" + $("#salary").val() + "</td></tr>";
+        $("tbody").after(display);
     });
 
-    var wb = XLSX.utils.book_new();
-
-    wb.Props = {
-        Title: "SheetJS Tutorial",
-        Subject: "Test",
-        Author: "Anthony Pham",
-        CreatedDate: new Date(2019, 12, 29)
-    };
-
-    wb.SheetNames.push("Test Sheet");
-
-    var ws = XLSX.utils.aoa_to_sheet(ws_data);
-
-    wb.Sheets["Test Sheet"] = ws;
-
-    var wbout = XLSX.write(wb, {
-        bookType: 'xlsx',
-        type: 'binary'
-    });
-
-    function s2ab(s) {
-        var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
-        var view = new Uint8Array(buf); //create uint8array as viewer
-        for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
-        return buf;
-    }
-
+    // Save as xls file 
     $("#button-a").click(function () {
+        var wb = XLSX.utils.book_new();
+        wb.Props = {
+            Title: "SheetJS Tutorial",
+            Subject: "Test",
+            Author: "Red Stapler",
+            CreatedDate: new Date(2017, 12, 19)
+        };
+
+        wb.SheetNames.push("Test Sheet");
+
+        var ws = XLSX.utils.aoa_to_sheet(ws_data);
+
+        wb.Sheets["Test Sheet"] = ws;
+
+        var wbout = XLSX.write(wb, {
+            bookType: 'xlsx',
+            type: 'binary'
+        });
+
+        function s2ab(s) {
+
+            var buf = new ArrayBuffer(s.length);
+            var view = new Uint8Array(buf);
+            for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
+            return buf;
+        }
+        console.log(ws_data);
         saveAs(new Blob([s2ab(wbout)], {
             type: "application/octet-stream"
-        }), 'test.xls');
+        }), 'test.xlsx');
     });
 });
